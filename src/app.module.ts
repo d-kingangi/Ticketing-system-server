bdab2065016990d683b5a224ebb50e1e03508847
d-kingangi <itsronduncan@gmail.com>
2025-06-24 07:07:27 +0300

    🛂 🛂 🛂 🛂

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from './config/config.module';
 import { DatabaseModule } from './database/database.module';
-import { ClientsModule } from './clients/clients.module';
-import { PatientsModule } from './patients/patients.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthModule } from './auth/jwt.module';
 import { APP_GUARD } from '@nestjs/core';
@@ -10,14 +8,6 @@ import { ClientContextMiddleware } from './common/middlewares/client-context.mid
 import { JwtClientGuard } from './auth/guards/jwt-client.guard';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './emails/emails.module';
-import { ReservationModule } from './reservation/reservation.module';
-import { QueuesModule } from './queues/queues.module';
-import { PersonnelModule } from './personnel/personnel.module';
-import { DepartmentsModule } from './departments/departments.module';
-import { ServicesModule } from './services/services.module';
-import { ChargesModule } from './charges/charges.module';
-import { InsuranceModule } from './insuarances/insurance.module';
-import { VisitTypeModule } from './visit-type/visit-type.module';
 
 @Module({
   imports: [
@@ -25,18 +15,8 @@ import { VisitTypeModule } from './visit-type/visit-type.module';
     DatabaseModule,
     JwtAuthModule, // Import JWT module first
     AuthModule,
-    ClientsModule,
-    PatientsModule,
     UsersModule,
-    EmailModule,
-    ReservationModule,
-    QueuesModule,
-    PersonnelModule,
-    DepartmentsModule,
-    ServicesModule,
-    ChargesModule,
-    InsuranceModule,
-    VisitTypeModule, // Ensure InsurancesModule is imported
+    EmailModule, // Ensure InsurancesModule is imported
   ],
   providers: [
     // Register JwtClientGuard as a global guard
