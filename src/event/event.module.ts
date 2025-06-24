7fb525a79c81f669140ba556a01d28311ab58ac1
d-kingangi <itsronduncan@gmail.com>
2025-06-24 10:58:59 +0300

    feat: update EventModule to include EventRepository and Mongoose integration

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { EventService } from './event.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EventController } from './event.controller';
+import { EventService } from './event.service';
+import { Event, EventSchema } from './entities/event.entity';
+import { EventRepository } from './event.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Event.name, schema: EventSchema }]),
+  ],
   controllers: [EventController],
-  providers: [EventService],
+  providers: [EventService, EventRepository], // Add EventRepository to providers
+  exports: [EventService, EventRepository],
 })
-export class EventModule {}
+export class EventModule { }
