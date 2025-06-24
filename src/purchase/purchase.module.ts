784daea46faff3402af1e3ed63a1d8b8337fa625
d-kingangi <itsronduncan@gmail.com>
2025-06-24 17:05:55 +0300

    feat: update PurchaseModule to include Mongoose integration and export PurchaseRepository

--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // Import MongooseModule
 import { PurchaseService } from './purchase.service';
 import { PurchaseController } from './purchase.controller';
+import { Purchase, PurchaseSchema } from './entities/purchase.entity'; // Import Purchase entity and schema
+import { PurchaseRepository } from './purchase.repository'; // Import PurchaseRepository
+import { EventModule } from '../event/event.module'; // Import EventModule as PurchaseService depends on EventService
+import { TicketTypeModule } from 'src/ticket-type/ticket-type.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Purchase.name, schema: PurchaseSchema }]),
+    EventModule,
+    TicketTypeModule,
+    UserModule,
+  ],
   controllers: [PurchaseController],
-  providers: [PurchaseService],
+  providers: [
+    PurchaseService,
+    PurchaseRepository, // Provide the PurchaseRepository
+  ],
+  exports: [PurchaseService, PurchaseRepository], // Export for other modules that might need them
 })
 export class PurchaseModule {}
