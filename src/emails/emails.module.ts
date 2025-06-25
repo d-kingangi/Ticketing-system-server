66aae62071714413e41ad18b3778d765413ee1e3
d-kingangi <itsronduncan@gmail.com>
2025-06-25 10:24:59 +0300

    refactor: remove forwardRef import and circular dependency from EmailModule

--- src/emails/emails.module.ts
+++ src/emails/emails.module.ts
@@ -1,13 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { EmailsController } from './emails.controller';
 import { EmailsService } from './emails.service';
-import { ClientsModule } from 'src/clients/clients.module';
 
 @Module({
   imports: [
     ConfigModule,
-    forwardRef(() => ClientsModule), // Use forwardRef to avoid circular dependency
   ],
   providers: [EmailsService],
   controllers: [EmailsController],
