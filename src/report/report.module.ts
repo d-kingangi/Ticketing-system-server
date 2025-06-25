3a2085797de7e919b3799d8162d706acbb20654e
d-kingangi <itsronduncan@gmail.com>
2025-06-25 08:52:04 +0300

    refactor: clean up ReportModule imports for better organization

--- src/report/report.module.ts
+++ src/report/report.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ReportService } from './report.service';
 import { ReportController } from './report.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventModule } from 'src/event/event.module';
+import { Purchase, PurchaseSchema } from 'src/purchase/entities/purchase.entity';
+import { PurchaseModule } from 'src/purchase/purchase.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Purchase.name, schema: PurchaseSchema },
+    ]),
+    PurchaseModule,
+    EventModule,
+  ],
   controllers: [ReportController],
   providers: [ReportService],
+
 })
 export class ReportModule {}
