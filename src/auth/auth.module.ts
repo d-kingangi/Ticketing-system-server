45143aa3440f6b41fd2b24924230b465ae98e0bc
d-kingangi <itsronduncan@gmail.com>
2025-06-26 06:10:57 +0300

    fix

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,12 +4,12 @@ import { JwtAuthModule } from './jwt.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { RolesGuard } from './guards/roles.guard';
-import { ClientAccessGuard } from './guards/client-access.guard';
 import { OwnershipGuard } from './guards/ownership.guard';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User, UserSchema } from './schema/user.schema';
 import { EmailModule } from 'src/emails/emails.module';
+import { OrganizationAccessGuard } from './guards/organization-access-guard';
 
 @Module({
   imports: [
@@ -23,8 +23,8 @@ import { EmailModule } from 'src/emails/emails.module';
     AuthService,
     JwtStrategy,
     RolesGuard,
-    ClientAccessGuard,
     OwnershipGuard,
+    OrganizationAccessGuard
   ],
   exports: [AuthService],
 })
