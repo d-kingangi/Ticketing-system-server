8bd6e7a8b1093526ae647d45abbb3aec4267cdc0
d-kingangi <itsronduncan@gmail.com>
2025-06-26 06:37:07 +0300

    fix

--- src/organization/organization.module.ts
+++ src/organization/organization.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { OrganizationService } from './organization.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { OrganizationController } from './organization.controller';
+import { OrganizationService } from './organization.service';
+import { Organization, OrganizationSchema } from './entities/organization.entity';
+import { OrganizationRepository } from './organization.repository';
+import { UsersModule } from 'src/users/users.module'; // Import UsersModule
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Organization.name, schema: OrganizationSchema }]),
+    UsersModule, // Import UsersModule to make UsersService available for injection
+  ],
   controllers: [OrganizationController],
-  providers: [OrganizationService],
+  providers: [OrganizationService, OrganizationRepository],
+  exports: [OrganizationService, OrganizationRepository], // Export OrganizationService for use in other modules (e.g., EventModule)
 })
-export class OrganizationModule {}
+export class OrganizationModule { }
