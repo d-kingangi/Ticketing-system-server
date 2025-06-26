bf97a75fb9cd6cf86008238b37917c3d39b9441b
d-kingangi <itsronduncan@gmail.com>
2025-06-26 15:16:05 +0300

    updated the event category module file

--- src/event-category/event-category.module.ts
+++ src/event-category/event-category.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose'; // Import MongooseModule
 import { EventCategoryService } from './event-category.service';
 import { EventCategoryController } from './event-category.controller';
+import { EventCategory, EventCategorySchema } from './entities/event-category.entity'; // Import the entity and schema
+import { EventCategoryRepository } from './event-category.repository'; // Import the repository
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: EventCategory.name, schema: EventCategorySchema },
+    ]),
+  ],
   controllers: [EventCategoryController],
-  providers: [EventCategoryService],
+  providers: [EventCategoryService, EventCategoryRepository],
+  exports: [EventCategoryService],
 })
 export class EventCategoryModule {}
