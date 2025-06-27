65f889cd2aa9319feea071b386c3ca79982071f6
d-kingangi <itsronduncan@gmail.com>
2025-06-27 12:02:31 +0300

    module file update

--- src/ticket-type/ticket-type.module.ts
+++ src/ticket-type/ticket-type.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TicketTypeService } from './ticket-type.service';
 import { TicketTypeController } from './ticket-type.controller';
+import { TicketType, TicketTypeSchema } from './entities/ticket-type.entity';
+import { TicketTypeRepository } from './ticket-type.repository';
+import { EventModule } from '../event/event.module'; // CHANGE: Import EventModule
+import { OrganizationModule } from '../organization/organization.module'; // CHANGE: Import OrganizationModule
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: TicketType.name, schema: TicketTypeSchema }]),
+    EventModule,
+    OrganizationModule,
+  ],
   controllers: [TicketTypeController],
-  providers: [TicketTypeService],
+  providers: [TicketTypeService, TicketTypeRepository],
+  exports: [TicketTypeService, TicketTypeRepository],
 })
 export class TicketTypeModule {}
