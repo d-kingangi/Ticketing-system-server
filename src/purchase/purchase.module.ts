52e907a513e12130a63d29d9a4f60a09db0724bf
d-kingangi <itsronduncan@gmail.com>
2025-06-27 14:56:45 +0300

    🐛 module file fixes

--- src/purchase/purchase.module.ts
+++ src/purchase/purchase.module.ts
@@ -1,12 +1,15 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose'; // Import MongooseModule
+// purchase.module.ts
+import { Module, forwardRef } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PurchaseService } from './purchase.service';
 import { PurchaseController } from './purchase.controller';
-import { Purchase, PurchaseSchema } from './entities/purchase.entity'; // Import Purchase entity and schema
-import { PurchaseRepository } from './purchase.repository'; // Import PurchaseRepository
-import { EventModule } from '../event/event.module'; // Import EventModule as PurchaseService depends on EventService
-import { TicketTypeModule } from 'src/ticket-type/ticket-type.module';
-import { UsersModule } from 'src/users/users.module';
+import { Purchase, PurchaseSchema } from './entities/purchase.entity';
+import { PurchaseRepository } from './purchase.repository';
+import { EventModule } from '../event/event.module';
+import { TicketTypeModule } from '../ticket-type/ticket-type.module';
+import { UsersModule } from '../users/users.module';
+import { DiscountModule } from '../discount/discount.module'; // CHANGE: Import the new DiscountModule
+import { TicketModule } from 'src/ticket/ticket.module';
 
 @Module({
   imports: [
@@ -14,12 +17,11 @@ import { UsersModule } from 'src/users/users.module';
     EventModule,
     TicketTypeModule,
     UsersModule,
+    DiscountModule, // CHANGE: Add DiscountModule to the imports array
+    forwardRef(() => TicketModule),
   ],
   controllers: [PurchaseController],
-  providers: [
-    PurchaseService,
-    PurchaseRepository, // Provide the PurchaseRepository
-  ],
-  exports: [PurchaseService, PurchaseRepository], // Export for other modules that might need them
+  providers: [PurchaseService, PurchaseRepository],
+  exports: [PurchaseService, PurchaseRepository],
 })
 export class PurchaseModule {}
