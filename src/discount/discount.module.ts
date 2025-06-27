aeea13906e05e0f7a1a6b8b3d90649bbaffeaeb1
d-kingangi <itsronduncan@gmail.com>
2025-06-27 10:47:29 +0300

    discount module file

--- src/discount/discount.module.ts
+++ src/discount/discount.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Discount, DiscountSchema } from './entities/discount.entity';
 import { DiscountService } from './discount.service';
+import { DiscountRepository } from './discount.repository';
 import { DiscountController } from './discount.controller';
+import { EventModule } from '../event/event.module'; // Import EventModule
+import { OrganizationModule } from '../organization/organization.module'; // Import OrganizationModule
+import { TicketTypeModule } from '../ticket-type/ticket-type.module'; // Import TicketTypeModule
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Discount.name, schema: DiscountSchema }]),
+    EventModule,
+    OrganizationModule,
+    TicketTypeModule,
+  ],
   controllers: [DiscountController],
-  providers: [DiscountService],
+  providers: [DiscountService, DiscountRepository],
+  exports: [DiscountService],
 })
 export class DiscountModule {}
