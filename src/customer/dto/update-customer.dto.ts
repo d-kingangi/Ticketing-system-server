b86784b4f8f970a4ef2059b668555410302a56fc
d-kingangi <itsronduncan@gmail.com>
2025-06-29 20:35:47 +0300

    fix: update import path for PartialType in UpdateCustomerDto

--- src/customer/dto/update-customer.dto.ts
+++ src/customer/dto/update-customer.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateCustomerDto } from './create-customer.dto';
 
+/**
+ * DTO for updating an existing customer.
+ * It extends the CreateCustomerDto, making all fields optional.
+ */
 export class UpdateCustomerDto extends PartialType(CreateCustomerDto) {}
