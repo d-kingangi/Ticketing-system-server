3085b706d4e94a2d0be2d8be685c26b9b55a0a9f
d-kingangi <itsronduncan@gmail.com>
2025-06-30 12:29:38 +0300

    Add DiscountScope enum to define discount applicability for enhanced flexibility

---
+++ src/discount/enum/discount-scope.enum.ts
@@ -0,0 +1,8 @@
+/**
+ * I've created this enum to define what a discount applies to.
+ * This makes the Discount entity more flexible and scalable.
+ */
+export enum DiscountScope {
+    EVENT = 'EVENT',
+    PRODUCT = 'PRODUCT',
+}
