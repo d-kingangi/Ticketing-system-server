46433bee00eff137b9a32efe6ea23c9d60b91a2b
d-kingangi <itsronduncan@gmail.com>
2025-06-30 09:14:15 +0300

    feat: update ProductCategoryModule to include Mongoose integration and repository provider

--- src/product-category/product-category.module.ts
+++ src/product-category/product-category.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
-import { ProductCategoryService } from './product-category/product-category.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductCategoryService } from './product-category.service';
 import { ProductCategoryController } from './product-category.controller';
+import {
+  ProductCategory,
+  ProductCategorySchema,
+} from './entities/product-category.entity';
+import { ProductCategoryRepository } from './product-category.repository';
 
 @Module({
-  providers: [ProductCategoryService],
-  controllers: [ProductCategoryController]
+  imports: [
+    MongooseModule.forFeature([
+      { name: ProductCategory.name, schema: ProductCategorySchema },
+    ]),
+  ],
+  controllers: [ProductCategoryController],
+  providers: [ProductCategoryService, ProductCategoryRepository],
+  exports: [ProductCategoryService],
 })
-export class ProductCategoryModule {}
+export class ProductCategoryModule { }
