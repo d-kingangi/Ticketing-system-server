07db0a05472a08edd15709eccc17c1059dc78687
d-kingangi <itsronduncan@gmail.com>
2025-06-30 11:03:41 +0300

    🦺 dto file

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,8 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
 
+/**
+ * DTO for updating an existing product.
+ * It extends the CreateProductDto, making all fields optional.
+ */
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
