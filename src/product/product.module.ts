32b1b0739e8e42e3f9007188ba1511aa367836b0
d-kingangi <itsronduncan@gmail.com>
2025-07-04 11:10:53 +0300

    fix: dependency injection

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,10 +4,12 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { Product, ProductSchema } from './entities/product.entity';
 import { ProductRepository } from './product.repository';
+import { ProductCategoryModule } from 'src/product-category/product-category.module'; // <-- Add this import
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+    ProductCategoryModule
   ],
   controllers: [ProductController],
   providers: [ProductService, ProductRepository],
