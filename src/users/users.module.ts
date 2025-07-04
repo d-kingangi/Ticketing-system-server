2d74ae5ba3c6ccee01bfa80b7bf925e13cc03384
d-kingangi <itsronduncan@gmail.com>
2025-07-04 10:53:26 +0300

    🐛 fix: dependecy injection error fix

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,19 +1,17 @@
-// src/users/users.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-// import { ClientsModule } from '../clients/clients.module';
 import { UserSchema } from 'src/auth/schema/user.schema';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UserRepository } from './users.repository'; // <-- Add this import
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    // ClientsModule,
   ],
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [UsersService, UserRepository], 
+  exports: [UsersService, UserRepository],  
 })
 export class UsersModule {}
