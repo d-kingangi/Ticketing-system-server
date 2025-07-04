e4de10433c8bd785597ef4f0951254b87d8fa061
d-kingangi <itsronduncan@gmail.com>
2025-07-04 10:34:20 +0300

    refactor: organize imports and improve module structure

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { CustomerService } from './customer.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CustomerController } from './customer.controller';
+import { CustomerService } from './customer.service';
+import { Customer, CustomerSchema } from './entities/customer.entity';
+import { CustomerRepository } from './customer.repository';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Customer.name, schema: CustomerSchema }]),
+  ],
   controllers: [CustomerController],
-  providers: [CustomerService],
+  providers: [CustomerService, CustomerRepository],
+  exports: [CustomerService, CustomerRepository],
 })
 export class CustomerModule {}
